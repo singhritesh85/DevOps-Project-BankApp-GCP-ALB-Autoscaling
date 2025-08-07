ab1879fa69f6ad3996ea25e15adc0166741d867c
Ritesh Kumar Singh <56765895+singhritesh85@users.noreply.github.com>
2025-08-07 15:18:27 +0530

    Delete terraform-gcp-global-autoscale-application-loadbalancer directory

--- terraform-gcp-global-autoscale-application-loadbalancer/main/output.tf
+++
@@ -1,4 +0,0 @@
-output "gcp_instance_template_vm_instance_autoscale_alb_mysql_gitlab_vm_instance_private_and_static_ip_gcp_alb_static_ip" {
-  description = "Details of the Google Cloud Instance Template, Instance Group, VM Instance, Autoscale, Application LoadBalancer and MySQL"
-  value       = "${module.autoscale_alb}"
-}
